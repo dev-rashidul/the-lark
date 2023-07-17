3f696326fb645bbfd28eaca93eeb4c289ab13fd5
dev-rashidul <rashidul.contact@gmail.com>
2023-07-17 23:09:24 +0600

    Project done

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+// Slider JavaScript
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  items: 1,
+  navText: [
+    '<img src="../assets/images/arrow-left-icon.svg">',
+    '<img src="../assets/images/arrow-right-icon.svg">',
+  ],
+});
+
+////
+
+let index = 1;
+
+const on = (listener, query, fn) => {
+	document.querySelectorAll(query).forEach(item => {
+		item.addEventListener(listener, el => {
+			fn(el);
+		})
+	})
+}
+
+on('click', '.selectBtn', item => {
+	const next = item.target.nextElementSibling;
+	next.classList.toggle('toggle');
+	next.style.zIndex = index++;
+});
+on('click', '.option', item => {
+	item.target.parentElement.classList.remove('toggle');
+
+	const parent = item.target.closest('.select').children[0];
+	parent.setAttribute('data-type', item.target.getAttribute('data-type'));
+	parent.innerText = item.target.innerText;
+})
